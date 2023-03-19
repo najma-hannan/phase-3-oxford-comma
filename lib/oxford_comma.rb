a4c9bcac32719d278209b54f2a9ffdc428fd4f66
Hannan <najma.mahat@student.moringaschool.com>
2023-03-19 07:54:17 +0300

    Completed oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+    if array.length <= 1
+        return array.join
+    elsif array.length == 2
+        return "#{array.slice(0, array.length - 1).join(", ")} and #{array.slice(-1)}"
+    else
+        return "#{array.slice(0, array.length - 1).join(", ")}, and #{array.slice(-1)}"
+    end
 end
